4d0d6e4f9e8aa5de169c6636d50f2bc8a86f75a7
Timothy Olaonipekun Omoniyi <olayinkatimothy102@gmail.com>
2024-01-15 20:26:49 +0100

    admin username faulty??

--- House Of Young/accounts/models.py
+++ House Of Young/accounts/models.py
@@ -1,8 +1,36 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
-from django.contrib.auth.models import User
-from django.db.models.signals import post_save
 
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, username, password=None, **extra_fields):
+        if not email:
+            raise ValueError('Users must have an email address.')
+
+        if not username:
+            raise ValueError('Users must have a username.')
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, username=username, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, username, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('email_is_verified', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        if extra_fields.get('email_is_verified') is not True:
+            raise ValueError('Superuser must have email_is_verified=True.')
+
+        return self.create_user(email, username, password, **extra_fields)
 
 class CustomUser(AbstractBaseUser):
     email = models.EmailField(max_length=254, unique=True)
@@ -15,11 +43,10 @@ class CustomUser(AbstractBaseUser):
     is_superuser = models.BooleanField(default=False)
     date_joined = models.DateTimeField(auto_now_add=True)
 
-
     USERNAME_FIELD = 'username'
     REQUIRED_FIELDS = ['email', 'first_name', 'last_name']
 
-    objects = BaseUserManager()
+    objects = CustomUserManager()
 
     def __str__(self):
         return self.username
